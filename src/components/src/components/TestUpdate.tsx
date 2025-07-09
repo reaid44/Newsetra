e46fc707a9ff6a8d7c4fd30e8a327228382c86dd
reaid44 <reaidhasanmd88@gmail.com>
2025-07-10 04:21:10 +0700

    Create TestUpdate.tsx

---
+++ src/components/src/components/TestUpdate.tsx
@@ -0,0 +1,9 @@
+export default function TestUpdate() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1 style={{ fontSize: "32px", fontWeight: "bold", color: "green" }}>
+        Update Test Successful!
+      </h1>
+    </div>
+  );
+}
